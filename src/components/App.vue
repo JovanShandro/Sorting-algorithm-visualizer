0976caeb8ab790ea9700e1f65bc4c7840c9a6799
JovanShandro <shandrojovan@gmail.com>
2020-01-08 22:28:27 +0100

    Add array generator and basic styling

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,21 +1,56 @@
 <template>
   <div>
-      <h1>Just a test</h1>
+    <h1>Visualize your favourite sorting algorithm</h1>
+    <button @click="generateRandomArray">Random</button>
+    <select v-model="choice">
+      <option disabled value>Select the Algorithm</option>
+      <option value="0">Merge Sort</option>
+      <option value="1">Selection Sort</option>
+      <option value="2">Bubble Sort</option>
+      <option value="3">Quick Sort</option>
+      <option value="4">Heap Sort</option>
+      <option value="5">Counting Sort</option>
+      <option value="6">Insertion Sort</option>
+      <option value="7">Radix Sort</option>
+    </select>
+    <button @click="sort">Sort</button>
+
+    <svg
+      width="500"
+      height="1300"
+      fill="#000"
+      :transform="'transform: translate(-50%)'"
+      :key="rerender"
+    >
+      <g v-for="(value, i) in array" :key="i" :transform="`translate(0, ${i * 13})`">
+        <rect height="10" :width="value" fill="#523456" />
+        <text fill="#ffffff" x="5" y="10">{{ value }}</text>
+      </g>
+    </svg>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      array: Array(100).fill(100),
+      rerender: 0,
+      choice: ""
+    };
+  },
+  methods: {
+    generateRandomArray() {
+      for (let i = 0; i < this.array.length; i++) {
+        this.array[i] = Math.floor(Math.random() * 500 + 1);
+      }
+      this.rerender++;
+    }
+  },
+  sort() {}
+};
 </script>
 
 <style>
-body {
-    font-weight: 500;
-    color:blue;
-}
-h1 {
-    text-align: center;
-}
 </style>
